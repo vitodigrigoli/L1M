e48cbd7145568aa1e7d8746bcfd33d89a67bd77f
Vito Di Grigoli <73437231+vitodigrigoli@users.noreply.github.com>
2024-03-25 17:06:00 +0100

    fixing navbar, dark mode, search bar, scroll animation, figures

--- scripts.js
+++ scripts.js
@@ -31,3 +31,42 @@ navbar__toggle.addEventListener('click', () => {
 	menu.classList.toggle('menu--open')
 	body.classList.toggle('overflow--hidden')
 })
+
+
+const dark_mode = document.querySelectorAll('.icon__dark_mode')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+    }
+});
+
+dark_mode.forEach(item => item.addEventListener('click', () => {
+    const wasDarkMode = document.body.classList.toggle('dark-mode');
+    // Aggiorna il localStorage con il nuovo stato
+    localStorage.setItem('darkMode', wasDarkMode);
+}));
+
+
+const watch = document.querySelectorAll('.watch')
+const intersection_observer = new IntersectionObserver( (items) =>{
+	items.forEach( item => {
+		
+		if( item.isIntersecting){
+			item.target.classList.add('in-view')
+		}
+		else{
+			item.target.classList.remove('in-view')
+		}
+	})
+}, { threshold: 0.1} )
+
+watch.forEach( el => intersection_observer.observe(el))
+
+
+const search = document.querySelector('.navbar__icons__search')
+const search_icon = document.querySelector('.navbar__icons__search i')
+search_icon.addEventListener('click', () =>{
+	search.classList.toggle('navbar__icons__search--active')
+})
